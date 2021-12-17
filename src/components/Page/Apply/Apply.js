a5d8139e03ce79f514d25fc67af2f649bbde4551
Jannat Borna <81918298+JannatBorna@users.noreply.github.com>
2021-12-17 19:29:35 +0600

    add

--- src/components/Page/Apply/Apply.js
+++ src/components/Page/Apply/Apply.js
@@ -1,31 +1,36 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useRef } from 'react';
+import useAuth from '../../../hook/useAuth';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { useParams } from 'react-router';
 import { useForm } from "react-hook-form";
 import swal from 'sweetalert';
-import useAuth from '../../../hook/useAuth';
 import './Apply.css'
+
+
+
+
 const Apply = () => {
 
     const { user } = useAuth();
     const { register } = useForm();
     const { servicesId } = useParams();
-    const [apply, setApply] = useState({})
-    
+    const [product, setProduct] = useState({});
 
     useEffect(() => {
         fetch(`https://warm-oasis-87609.herokuapp.com/services/${servicesId}`)
-        .then(res => res.json())
-        .then(data => setApply(data))
-      
+            .then(res => res.json())
+            .then(data => setProduct(data))
     }, [servicesId])
+
 
     const nameRef = useRef();
     const imgRef = useRef();
     const addressRef = useRef();
     const PhoneNumberRef = useRef();
-    const servicesRef = useRef();
+    const productsRef = useRef();
     const priceRef = useRef();
-    const descriptionRef = useRef();
+    const describeRef = useRef();
 
 
     const handleUser = e => {
@@ -34,50 +39,64 @@ const Apply = () => {
         const img = imgRef.current.value;
         const address = addressRef.current.value;
         const phoneNumber = PhoneNumberRef.current.value;
-        const services = servicesRef.current.value;
+        const products = productsRef.current.value;
         const price = priceRef.current.value;
-        const description = descriptionRef.current.value;
+        const describe = describeRef.current.value;
 
-        const newUser = { phoneNumber, address, services, name, img, price, description };
 
-        // fetch('https://desolate-sea-37549.herokuapp.com/orders', {
-        //     method: 'POST',
-        //     headers: {
-        //         'content-type': 'application/json'
-        //     },
-        //     body: JSON.stringify(newUser)
-        // })
-        //     .then(res => res.json())
-        //     .then(data => {
-        //         if (data.insertedId) {
-        //             swal("Good job!", "Order Place Successfully!", "success");
-        //             e.target.reset();
+        const newUser = { phoneNumber, address, products, name, img, price, describe };
 
-        //         }
-        //     })
-        // e.preventDefault();
+        fetch('https://warm-oasis-87609.herokuapp.com/orders', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newUser)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.insertedId) {
+                    swal("Good job!", "Place Successfully!", "success");
+                    e.target.reset();
 
+                }
+            })
+        e.preventDefault();
 
 
     }
 
+
+
+    
+
     return (
         <div>
-            <h2 className="">place Now</h2>
+            <h2 className="my-4">place Order</h2>
+
+
             <div className="order-product">
                 <form onSubmit={handleUser}>
 
                     <input type="text" placeholder="Name" value={user?.displayName} />
                     <input type="email" placeholder="Email" value={user?.email} />
-                    <input type="product name" placeholder="Product Name" ref={servicesRef} value={apply?.name} />
-                    <input type="img" placeholder="Product img" ref={imgRef} value={apply?.img} />
-                    <input type="price" placeholder="Price" ref={priceRef} value={apply?.price}/>
-                    <textarea {...register("description")} placeholder="description" ref={descriptionRef} value={apply?.description} />
+                    <input type="product name" placeholder="Product Name" ref={productsRef} value={product?.name} />
+                    <input type="name" placeholder="Product Name" ref={nameRef} value={product?.name} />
+                    <input type="img" placeholder="Product img" ref={imgRef} value={product?.img} />
+                    <input type="price" placeholder="Price" ref={priceRef} value={product?.price} />
+
+                    <textarea {...register("describe")} placeholder="describe" ref={describeRef} value={product?.describe} />
                     <br />
                     <input type="address" placeholder="Your Address" ref={addressRef} />
                     <input type="Phone" placeholder="Phone no." ref={PhoneNumberRef} />
                     <input className="btn btn-secondary" type="submit" value="Order Place" />
                 </form>
+
+
+
+
+
+
             </div>
         </div>
     );
