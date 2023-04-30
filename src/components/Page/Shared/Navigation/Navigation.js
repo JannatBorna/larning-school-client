9a9cfc93ec91e49966d9b5ac900bda1e1c99cbef
Dilruba Jannat Borna <bornadilruba38@gmail.com>
2023-04-30 22:08:04 +0600

    add

--- src/components/Page/Shared/Navigation/Navigation.js
+++ src/components/Page/Shared/Navigation/Navigation.js
@@ -1,12 +1,11 @@
+import React from 'react';
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import {
   faFacebookSquare,
-  faInstagramSquare,
   faTwitterSquare,
   faYoutube
 } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from 'react';
-import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { HashLink } from "react-router-hash-link";
 import useAuth from "../../../../hook/useAuth";
 import "./Navigation.css";
@@ -16,44 +15,36 @@ import "./Navigation.css";
 const Navigation = () => {
   const { user, logOut } = useAuth();
   return (
-    <div id="navigation">
-      <Navbar bg="light" expand="lg" sticky="top">
-        <Container fluid>
-             <Navbar.Brand href="#">
-       <Nav className="justify-content-start">
-       <Nav.Link className="instagram" href="https://www.instagram.com/borna_jannat">
-         <FontAwesomeIcon icon={faInstagramSquare} /> 
-       </Nav.Link>  
-      
-       <Nav.Link className="twitter" href="https://twitter.com/jannat_borna">
-         <FontAwesomeIcon icon={faTwitterSquare} />
-       </Nav.Link>  
+    <div>
+      <Navbar bg="light" expand="lg" fixed="top">
+        <Container>
+          <Navbar.Brand href="#">
+            <Nav className="d-flex justify-content-start">
+              <Nav.Link className="twitter" href="https://twitter.com/jannat_borna">
+                <FontAwesomeIcon icon={faTwitterSquare} />
+              </Nav.Link>  
      
-       <Nav.Link className="youtube" href="https://www.youtube.com">
-         <FontAwesomeIcon icon={faYoutube} />
-       </Nav.Link> 
+              <Nav.Link className="youtube" href="https://www.youtube.com">
+                <FontAwesomeIcon icon={faYoutube} />
+              </Nav.Link> 
       
-       <Nav.Link className="facebook" href="https://www.facebook.com/Bornabaset">
-         <FontAwesomeIcon icon={faFacebookSquare} />
-       </Nav.Link>
-     </Nav> 
-   </Navbar.Brand>
+              <Nav.Link className="facebook" href="https://www.facebook.com/Bornabaset">
+                <FontAwesomeIcon icon={faFacebookSquare} />
+              </Nav.Link>
+            </Nav> 
+          </Navbar.Brand>
 
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link className="nav_bar active mx-5" as={HashLink} to="/home#home">Home</Nav.Link>
-            <Nav.Link className="nav_bar mx-3" as={HashLink} to="/ourServices#ourServices">Our services</Nav.Link>
-            <Nav.Link className="nav_bar mx-5" as={HashLink} to="/dashboard#dashboard">Dashboard</Nav.Link>
+          <Nav className="mid-nav" navbarScroll >
+            <Nav.Link className="nav-bar active mx-5" as={HashLink} to="/home#home">Home</Nav.Link>
+            <Nav.Link className="nav-bar mx-3" as={HashLink} to="/ourServices#ourServices">Our services</Nav.Link>
+            <Nav.Link className="nav-bar mx-5" as={HashLink} to="/dashboard#dashboard">Dashboard</Nav.Link>
           </Nav>
                         
            {user?.email ? (   
               
-            <Navbar.Collapse className="justify-content-end ">
+            <Navbar.Collapse className="justify-content-end">
               <NavDropdown className="text justify-content-end " title="Signed in as:" id="basic-nav-dropdown">
                 <div>
                     <p className="text-dark fw-bolder mx-4" href="#login">
@@ -76,7 +67,7 @@ const Navigation = () => {
         ) : (
                     
           <Navbar.Collapse className="justify-content-end">
-            <Nav.Link className="nav_login" as={HashLink} to="/login#login">
+            <Nav.Link className="nav-login" as={HashLink} to="/login#login">
               Login  
             </Nav.Link> 
           </Navbar.Collapse>       
